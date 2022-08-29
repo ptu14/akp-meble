96a18e0da2369222bc5b6e914d36c2a6389d45dc
Mateusz Daniluk <mateusz.daniluk@comarch.pl>
2022-08-29 17:13:02 +0200

    day-one end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Product } from './model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Modne i wygodne krzesło Ygstadd',
+      shopName: 'Meble Boguś',
+      image: './assets/img/furniture-1.jpg',
+      price: 100,
+      priceOld: 200,
+      itemsLeft: 123,
+      description:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, nisi sed. Nisi quibusdam voluptates blanditiis omnis tempora provident sunt cumque!',
+    },
+  ];
+}
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../model';
 
 @Component({
   selector: 'akp-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {}
+export class ProductComponent implements OnInit {
+  @Input()
+  product!: Product;
+
+  discount: number = 0;
+
+  ngOnInit() {
+    if (this.product.priceOld) {
+      this.discount = this.product.price - this.product.priceOld;
+    }
+  }
+}
